0fbd13ceada54c9e33970b8f623bf69d2e6110f9
Ryan Nóbrega Brandão <ryanbrandao18@gmail.com>
2023-11-02 23:03:17 -0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,78 @@
+/*!
+* Start Bootstrap - Agency v7.0.12 (https://startbootstrap.com/theme/agency)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    //  Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+const targetDate = new Date('2024-12-14T00:00:00').getTime();
+function updateCountdown() {
+  // Obter a data e hora atual
+  const currentDate = new Date().getTime();
+
+  // Calcular a diferença entre as datas
+  const difference = targetDate - currentDate;
+
+  // Cálculos para dias, horas, minutos e segundos
+  const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+
+  // Exibir a contagem regressiva no elemento HTML
+  const countdownElement = document.getElementById('countdown');
+  countdownElement.innerHTML = `
+    Faltam ${days} dias, ${hours} horas, ${minutes} minutos, ${seconds} segundos para 14/12/2024
+  `;
+}
+
+// Chamar a função inicialmente e atualizá-la a cada segundo
+updateCountdown();
+setInterval(updateCountdown, 1000);
